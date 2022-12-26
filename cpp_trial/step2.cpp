5bfea771915e4b31f4009db147ffd490d1824627
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-26 17:19:41 +0900

    ＃02:はじめてのプログラミング

---
+++ cpp_trial/step2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello world" <<endl;
+    // cout(シーアウト)
+    // 指定したデータを出力する部品(オブジェクト)
+    // << (出力演算子)
+    // 右のデータを左のオブジェクトにわたす
+
+    cout << "Hello " << "Paiza" <<endl;
+    cout << "冒険に出かけよう" << endl;
+    // endl
+    // 改行
+}
