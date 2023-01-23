e2b89994ca0ce87e0012fe70f791e9c920d83dc5
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-23 16:57:58 +0900

    ＃04:コメントを書く_演習

---
+++ cpp_trial/step4_ex.cpp
@@ -0,0 +1,16 @@
+// コメントアウトを解除する
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello c++" << endl;
+  cout << "Hello paiza" << endl;
+}
+// // コメントアウトする
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+// //   cout << "Hello world" << endl;
+//   cout << "Hello c++" << endl;
+// }
