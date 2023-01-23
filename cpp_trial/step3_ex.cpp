951f58e5e51afb0559335e9f4d7d0502c691ea9e
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-23 16:51:18 +0900

    ＃03:間違いやすいポイント_演習

---
+++ cpp_trial/step3_ex.cpp
@@ -0,0 +1,28 @@
+// メッセージを表示しよう 4
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello paiza" << endl;
+}
+// メッセージを表示しよう 3
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello paiza" << endl;
+// }
+// メッセージを表示しよう 2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello paiza" << endl;
+// }
+// メッセージを表示しよう 1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello paiza" << endl;
+// }
