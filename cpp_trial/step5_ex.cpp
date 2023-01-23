64b3b60d24fabe6b5d9eccd7453f5a0aa9b96286
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-23 18:01:05 +0900

    ＃05:数値を扱う_演習

---
+++ cpp_trial/step5_ex.cpp
@@ -0,0 +1,22 @@
+// 数式を表示しよう3
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "12345 + 6789" << endl;
+}
+// 足し算しよう2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << 12345 + 6789 << endl;
+// }
+
+// 数値を表示しよう1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "12345" << endl;
+// }
