ebb6bf7e972f25351126e9cda54fd628e98647b1
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-23 16:41:39 +0900

    ＃02:はじめてのプログラミング_演習

---
+++ cpp_trial/step2_ex.cpp
@@ -0,0 +1,25 @@
+// メッセージを表示しよう 3
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "世界の皆さん、こんにちは" << endl;
+}
+
+// メッセージを表示しよう 2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello paiza" << endl;
+//   cout << "Good morning" << endl;
+
+// }
+
+// メッセージを表示しよう 1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello paiza" << endl;
+// }
