20ea3b5c1f1daf7043ffa8fd12f135b14bc0ce16
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-23 17:55:44 +0900

    ＃05:数値を扱う

---
+++ cpp_trial/step5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello world" << endl;
+  cout << "100" << endl;//文字としての100
+  cout << 100 << endl;  //数値としての100
+  cout << 100 + 30 << endl; //四則計算
+  cout << 100 + 30 << "円" << endl; //文字連結
+  cout << "100 + 30" << endl; //計算式
+}
