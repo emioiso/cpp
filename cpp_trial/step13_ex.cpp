e0be9b84f4b5dda83a571fba8083bde9b5111d98
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 05:15:30 +0900

    ＃13:同じ処理を繰り返す_演習

---
+++ cpp_trial/step13_ex.cpp
@@ -0,0 +1,12 @@
+// 10 回繰り返す
+#include <iostream>
+using namespace std;
+
+int main() {
+  string greeting = "Hello paiza";
+//   cout << greeting << endl;
+
+	for(int i = 0; i < 10; i++){
+		cout << greeting << endl;
+	}
+}
