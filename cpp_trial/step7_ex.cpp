fd04a1cd8eac99952cb49c54644fa4ed9cae7248
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 04:07:37 +0900

    ＃07:変数にデータを入れる_演習

---
+++ cpp_trial/step7_ex.cpp
@@ -0,0 +1,36 @@
+// 変数で数値を表示しよう
+#include <iostream>
+using namespace std;
+
+int main() {
+  int number = 12345;
+  cout << number << endl;
+}
+
+// // 間違いさがし
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   string greeting = "Hello paiza";
+//   cout << greeting << endl;
+// }
+
+// 変数でメッセージを表示しよう1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   string greeting = "Hello paiza";
+//   cout << greeting << endl;
+//   cout << greeting << endl;
+
+// }
+// 変数でメッセージを表示しよう1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   string greeting = "Hello paiza";
+//   cout << greeting << endl;
+// }
