edf2c189614c7ad435441b97c0d1ff16a7a1d4fc
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 04:03:41 +0900

    ＃07:変数にデータを入れる

---
+++ cpp_trial/step7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//stringは文字列を表すデータ型
+	string greeting = "Hello world";
+	cout << greeting << endl;
+
+//2回目のstringは省略できる
+	greeting = "Hello paiza";
+	cout << greeting << endl;
+
+	int number = 100;
+	cout << number << endl;
+}
