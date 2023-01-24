aaec5a8fda108309390e9798642e52d96e3f3858
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 05:11:41 +0900

    ＃13:同じ処理を繰り返す

---
+++ cpp_trial/step13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string greeting = "Hello paiza";
+//   cout << greeting << endl;
+//   cout << greeting << endl;
+//   cout << greeting << endl;
+
+	for(int i = 0; i < 3; i ++){
+		cout << greeting << i << endl;
+	}
+}
+// ===== 理解したこと =====
+// for文のiの出力の書き方。
+// これでデバックができるようになる！
