60abe57c4b3473a022ba45a18ef9c3b58f5c5c29
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 04:14:40 +0900

    ＃08:データを受け取る_演習

---
+++ cpp_trial/step8_ex.cpp
@@ -0,0 +1,20 @@
+// 整数データを受け取って表示しよう
+#include <iostream>
+using namespace std;
+
+int main() {
+  int number;
+  cin >> number;
+  cout << number << endl;
+}
+
+// データを受け取って表示しよう - その1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   string message;
+
+//   cin >> message;
+//   cout << message << endl;
+// }
