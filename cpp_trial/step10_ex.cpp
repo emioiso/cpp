368c5ba199212ba234bc3512a72a0e28e9dfec49
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 04:32:39 +0900

    ＃10:条件に一致したら処理を実行する

---
+++ cpp_trial/step10_ex.cpp
@@ -0,0 +1,41 @@
+// 特定の条件で数値を表示
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cin >> number;
+//   cout <<  number << endl;
+
+	if(number <= 100){
+		cout << number << endl;
+	}
+}
+// // 特定の条件で数値を表示
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int number;
+//   cin >> number;
+// //   cout << number << endl;
+
+//   if(number >= 10){
+//       cout << number << endl;
+//   }
+// }
+
+// // // 文字列に一致しているか判定する
+// // #include <iostream>
+// // using namespace std;
+
+// // int main() {
+// //   string name;
+// //   cin >> name;
+// // //   cout << name << endl;
+
+// //   if(name == "Hello"){
+// //       cout << "こんにちは" << endl;
+// //   }
+
+// // }
