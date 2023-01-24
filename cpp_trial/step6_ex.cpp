b46aee5b4803540edef37d492784e5a0daca70ae
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 03:53:37 +0900

    ＃06:プログラムで計算する_演習

---
+++ cpp_trial/step6_ex.cpp
@@ -0,0 +1,21 @@
+// 割り算しよう3
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 12345 / 5;
+}
+// 掛け算しよう2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << 12345 * 6789;
+// }
+// 引き算しよう1
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << 12345 - 6789;
+// }
