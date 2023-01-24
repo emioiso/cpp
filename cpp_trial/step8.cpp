43dda639e51929a9d199424443d1200ca5281726
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 04:11:42 +0900

    ＃08:データを受け取る

---
+++ cpp_trial/step8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name;
+  cin >> name;
+  cout << "Hello " << name << endl;
+
+  int number;
+  cin >> number;
+  cout << number << endl;
+  cout << number * 100<< endl;
+}
