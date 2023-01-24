f53dc667b334061f13a84bb2cbff4d382e5176ab
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-25 05:20:16 +0900

    ＃14:処理の繰り返し回数を指定する

---
+++ cpp_trial/step14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int count;
+	cin >> count;
+	cout << count << endl;
+
+	string greeting = "Hello world";
+
+	for(int i = 0; i < count; i++){
+		cout << greeting << endl;
+	}
+}
